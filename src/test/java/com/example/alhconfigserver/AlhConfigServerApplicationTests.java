0159865cb77da466fdf4faacafd52936a4b972f0
Niconiconi233 <1660994874@qq.com>
2019-10-27 13:14:35 +0800

    add

---
+++ src/test/java/com/example/alhconfigserver/AlhConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alhconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlhConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
